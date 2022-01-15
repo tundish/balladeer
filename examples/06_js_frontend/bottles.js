ec4e15958135f7811c3685ffa12f77bda6c97c7c
tundish <tundish@gigeconomy.org.uk>
2022-01-15 16:02:09 +0000

    Create separate component for bottles.

--- examples/06_js_frontend/bottles.js
+++ examples/06_js_frontend/bottles.js
@@ -22,10 +22,7 @@ props: {
 template:  `
             <ul style="display: flex;">
             <li v-for="bottle in unbroken" style="flex-direction: row;">
-            <div class="bottle">
-                <img src="/img/bottle.svg" alt="A green bottle" style="width: 3rem;"/>
-                {{ bottle.colour }}
-            </div>
+            <avatar v-bind:bottle="bottle"></avatar>
             </li>
             </ul>`,
 data() {
@@ -36,6 +33,27 @@ computed: {
     unbroken() {
         return this.population.filter(bottle => bottle._states.Fruition.value == 1);
     }
+}
+});
+
+
+app.component("avatar", {
+props: {
+    bottle: {
+        type: Object,
+        required: true
+    }
+},
+template:  `
+            <div class="bottle">
+                <img src="/img/bottle.svg" alt="A green bottle" style="width: 3rem;"/>
+                {{ bottle.colour }}
+            </div>`,
+data() {
+    return {
+    }
+},
+computed: {
 }
 });
 
