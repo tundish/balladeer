5e2b0d811476074b585382a35a3ceda00d59da94
tundish <tundish@gigeconomy.org.uk>
2023-03-02 18:01:44 +0000

    First loader function working.

--- balladeer/lite/assets.py
+++ balladeer/lite/assets.py
@@ -2,6 +2,15 @@
 # encoding: utf-8
 
 import importlib.resources
+import tomllib
 
-f = [importlib.resources.as_file(p) for p in importlib.resources.files("balladeer.lite").joinpath(".").iterdir()]
-print(*list(f), sep="\n")
+
+def loader(package, resource=".", suffixes=[".dlg.toml"]):
+    for path in importlib.resources.files(package).joinpath(resource).iterdir():
+        if "".join(path.suffixes) in suffixes:
+            with importlib.resources.as_file(path) as f:
+                text = f.read_text(encoding="utf8")
+                yield tomllib.loads(text)
+
+assets = loader("balladeer.lite")
+print(*list(assets), sep="\n")
