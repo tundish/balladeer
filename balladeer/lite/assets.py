3a3ecd2b352a2cf807c72416734d92398adaecb8
tundish <tundish@gigeconomy.org.uk>
2023-03-03 08:13:14 +0000

    Sketching underscore as anonymous sequence.

--- balladeer/lite/assets.py
+++ balladeer/lite/assets.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import importlib.resources
+import pprint
 import tomllib
 
 
@@ -10,7 +11,19 @@ def loader(package, resource=".", suffixes=[".dlg.toml"]):
         if "".join(path.suffixes) in suffixes:
             with importlib.resources.as_file(path) as f:
                 text = f.read_text(encoding="utf8")
-                yield (resource, path, tomllib.loads(text))
+                try:
+                    tables = tomllib.loads(text)
+                    error = None
+                except tomllib.TOMLDecodeError as e:
+                    tables = None
+                    error = e
+                yield (resource, path, text, tables, error)
 
 assets = loader("balladeer.lite")
-print(*list(assets), sep="\n")
+
+def checker():
+    # lower case the sections: S, IF, DO
+    yield
+
+for a in assets:
+    pprint.pprint(a)
