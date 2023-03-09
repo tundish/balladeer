28d8c3ce029aa3b9852195a923b0b9cad521728d
tundish <tundish@gigeconomy.org.uk>
2023-03-09 18:29:24 +0000

    First stab at a speech markup.

---
+++ balladeer/lite/test/test_syntax.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#   encoding: utf8
+
+"""
+STAFF
+
+GUEST
+
+PHONE
+
+
+"""
+
+"<GUEST>" == "<GUEST:says>"
+
+
+Animation of entity
+"""
+<GUEST/entering:decides?pause=1&dwell=0.2#a>
+
+    a. Hello!
+    b. Say nothing
+
+"""
+
+"""
+<PHONE:alerts@GUEST>
+
+<GUEST>
+
+Your phone's ringing.
+
+<STAFF> How strange I didn't hear it.
+
+<PHONE:alerts@GUEST,STAFF>
+
+<STAFF> Oh, now I do!
+
+<PHONE/throbbing:alerts@GUEST,STAFF> RIIING RIIING!
+
+""
+
