e22198e5110854d65031328c1938918347dea4dd
tundish <tundish@gigeconomy.org.uk>
2023-03-11 13:55:22 +0000

    Add copyright.

--- balladeer/lite/test/test_speechmark.py
+++ balladeer/lite/test/test_speechmark.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
-#   encoding: utf8
+#   encoding: utf-8
+
+# This is part of the Balladeer library.
+# Copyright (C) 2023 D E Haynes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import re
 import tomllib
@@ -97,18 +113,10 @@ Your phone's ringing.
 # <code>:  `
 # <a>: [label](url)
 
-(
-    "Head",
-    ("propose", "confirm", "counter", "abandon", "condemn", "declare"),
-    defaults=(tuple(), tuple(), tuple(), tuple(), tuple(), tuple())
-)
+Head = ("propose", "confirm", "counter", "abandon", "condemn", "declare")
 
 
-(
-    "Hand",
-    ("decline", "suggest", "promise", "disavow", "deliver"),
-    defaults=(tuple(), tuple(), tuple(), tuple(), tuple())
-)
+Hand = ("decline", "suggest", "promise", "disavow", "deliver")
 
 
 class Syntax(unittest.TestCase):
