cdb277202c48257eabce359bc94ab1b3a414bc22
tundish <tundish@gigeconomy.org.uk>
2023-03-24 16:23:46 +0000

    New character for dlg_key.

--- balladeer/lite/director.py
+++ balladeer/lite/director.py
@@ -31,7 +31,7 @@ class Director:
         n = 1 if "type" in entity else 0
         return len(entity.get("states", [])) + n
 
-    def __init__(self, shot_key="_", dlg_key="-"):
+    def __init__(self, shot_key="_", dlg_key="D"):
         self.shot_key = shot_key
         self.dlg_key = dlg_key
 
