56399ec5edc0e153d5b1887c869fb81e2c8744de
Tundish <tundish@gigeconomy.org.uk>
2023-04-19 13:39:30 +0100

    Add first test for Grouping.

---
+++ balladeer/lite/test/test_types.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#   encoding: utf-8
+
+# This is part of the Balladeer library.
+# Copyright (C) 2023 D E Haynes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import unittest
+
+from balladeer.lite.entity import Entity
+from balladeer.lite.types import Grouping
+
+
+class GroupingTests(unittest.TestCase):
+
+    def test_grouping_by_class(self):
+        class A: pass
+        class B: pass
+
+        g = Grouping.typewise([A(), B(), A()])
+        self.assertIn(A, g)
+        self.assertEqual(2, len(g[A]))
+
+        self.assertIn(B, g)
+        self.assertEqual(1, len(g[B]))
+ 
