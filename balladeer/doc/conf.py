392c380863ea2d30363c8148dc00ea81158d2731
tundish <tundish@gigeconomy.org.uk>
2023-06-02 21:46:58 +0100

    Add Sphinx config and fix pinning.

--- balladeer/doc/conf.py
+++ balladeer/doc/conf.py
@@ -3,26 +3,36 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import pathlib
+import pkgutil
+import sys
+
+try:
+    import balladeer
+except ImportError:
+    sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Balladeer'
-copyright = '2023, D E Haynes'
-author = 'D E Haynes'
-release = '0.5.0'
+project = "Balladeer"
+copyright = "2023, D E Haynes"
+author = "D E Haynes"
+release = pkgutil.resolve_name("balladeer.__version__")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build",]
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "karma_sphinx_theme"
+html_static_path = ["_static"]
+html_title = f"{project} {release}"
