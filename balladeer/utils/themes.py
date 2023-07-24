714613b7e9964508482677feb3fa6f0466a681f3
Tundish <tundish@gigeconomy.org.uk>
2023-07-24 18:48:31 +0100

    Add a script to view themes.

---
+++ balladeer/utils/themes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#   encoding: utf-8
+
+# This is part of the Balladeer library.
+# Copyright (C) 2023 D E Haynes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
+import pathlib
+import sys
+
+from balladeer.lite.types import Page
+
+
+def parser(usage=__doc__):
+    rv = argparse.ArgumentParser(usage)
+    return rv
+
+
+def main(args):
+    page = Page()
+    print(page.html)
+    return 0
+
+
+def run():
+    p = parser()
+    args = p.parse_args()
+    rv = main(args)
+    sys.exit(rv)
+
+if __name__ == "__main__":
+    run()
