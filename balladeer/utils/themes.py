fa220b5295595c92cc2b6a4b79ee9e8cacd3f2e5
Tundish <tundish@gigeconomy.org.uk>
2023-07-24 19:38:07 +0100

    Start to parse theme colours.

--- balladeer/utils/themes.py
+++ balladeer/utils/themes.py
@@ -19,10 +19,22 @@
 
 import argparse
 import pathlib
+import re
 import sys
+import textwrap
 
 from balladeer.lite.types import Page
 
+def swatch(name, theme):
+    fn_regex = re.compile("(?P<fn>[^\(]+)\([^\)]*\)")
+    yield f"<h3>{name}</h3>"
+    yield "<ul>"
+    for label, value in theme.items():
+        fn = fn_regex.match(value)
+        yield "<li>"
+        yield f"{label}: {value} {fn.groupdict()}"
+        yield "</li>"
+    yield "</ul>"
 
 def parser(usage=__doc__):
     rv = argparse.ArgumentParser(usage)
@@ -31,6 +43,18 @@ def parser(usage=__doc__):
 
 def main(args):
     page = Page()
+    style = textwrap.dedent("""
+    <style>
+    body {
+    background-color: silver;
+    }
+    </style>
+    """).strip()
+    page.paste(page.zone.style, style)
+    for cls in [Page] + Page.__subclasses__():
+        page.paste(page.zone.body, f"<h2>{cls.__name__}</h2>")
+        for name, theme in cls.themes.items():
+            page.paste(page.zone.body, *swatch(name, theme))
     print(page.html)
     return 0
 
